e4f78f449dc8b2d7962d95fcf3950ededf505f6d
Konloch <konloch@gmail.com>
2023-12-16 23:16:44 -0700

    Resolve User NPE / Added Private Message Handling

--- src/main/java/com/konloch/ircbot/message/text/impl/PrivMSG.java
+++ src/main/java/com/konloch/ircbot/message/text/impl/PrivMSG.java
@@ -5,6 +5,8 @@ import com.konloch.ircbot.server.Channel;
 import com.konloch.ircbot.server.Server;
 import com.konloch.ircbot.server.User;
 
+import java.util.Arrays;
+
 import static com.konloch.util.FastStringUtils.split;
 
 /**
@@ -17,26 +19,38 @@ public class PrivMSG implements TextMessageEvent
 	public void handle(Server server, String[] splitPartMessage)
 	{
 		String nickname = split(splitPartMessage[0].substring(1), "!", 2)[0];
-		String location = splitPartMessage[2];
+		String[] locations = split(splitPartMessage[2], ",");
 		String msg = splitPartMessage[3].substring(1);
 		
-		if(location.startsWith("#"))
+		for(String location : locations)
 		{
-			Channel channel = server.get(location);
-			
-			if (channel != null)
+			if (location.startsWith("#"))
 			{
-				channel.setJoined(true);
+				Channel channel = server.get(location);
 				
-				User user = channel.get(nickname);
+				if (channel != null)
+				{
+					channel.setJoined(true);
+					
+					User user = channel.get(nickname);
+					
+					//create a temporary user object if we can't retrieve it from the name list
+					//TODO not entirely sure why this is happening, the RPL_NAMEPLY should handle this for us
+					if (user == null)
+						user = new User(server, nickname);
+					
+					//call on listener event
+					server.getBot().getListeners().callChannelMessage(channel, user, msg);
+				}
+			}
+			else
+			{
+				//create new user object
+				User user = new User(server, nickname);
 				
 				//call on listener event
-				server.getBot().getListeners().callChannelMessage(channel, user, msg);
+				server.getBot().getListeners().callPrivateMessage(user, msg);
 			}
-		}
-		else
-		{
-			//TODO handle pm
 		}
 	}
 }
